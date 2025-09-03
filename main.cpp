08f6505a9ac0fb635b6ef96650565b2927dae545
jo <wgjosiah@gmail.com>
2025-09-03 15:47:05 +0000

    code assignment.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 
 int main() {
- 
+    int height = 61;
+    int weight= 130;
 
+ double kg = weight * 0.453592;
+
+ double m= height * 0.0254;
+
+ double bmi = kg / (m * m);
+
+ cout << "The BMI for this person is : " << bmi << "." << endl; 
 
 }
